a9395b7ab752d625908ebfc46103a970e35c9478
Sharon Templin <sharon@hollygo.es>
2021-03-13 23:36:43 -0600

    Bonus question started.  Added image file of ERD.

---
+++ QuickDBD-Free Diagram-SQL-Challenge.sql
@@ -0,0 +1,67 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/WeSRNv
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" int   NOT NULL,
+    "dept_name" string   NOT NULL
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" int   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" int   NOT NULL,
+    "emp_no" int   NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" int   NOT NULL,
+    "birth_date" varchar   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "sex" varchar   NOT NULL,
+    "hire_date" varchar   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" int   NOT NULL,
+    "title" varchar   NOT NULL
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+CREATE INDEX "idx_departments_dept_no"
+ON "departments" ("dept_no");
+
+CREATE INDEX "idx_employees_emp_no"
+ON "employees" ("emp_no");
+
+CREATE INDEX "idx_titles_title_id"
+ON "titles" ("title_id");
+
